a0bafe9ff349d8b5a5947e395c9aae4bb705c464
Iván Pérez <iperez@itelligent.es>
2023-11-30 16:06:52 +0100

    Agregar archivos de proyecto.

---
+++ SINAC.Capacitacion.Curso1.Web/Controllers/DemoController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SINAC.Capacitacion.Curso1.Web.Controllers
+{
+    public class DemoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ SINAC.Capacitacion.Curso1.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SINAC.Capacitacion.Curso1.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
